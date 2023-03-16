d2b2d8619bb3690b528775d4b73c2f178fcf6cf3
RempelOliveira <rempel.oliveira@gmail.com>
2023-03-15 22:11:33 -0300

    [MB-2] - Docs

--- Main.py
+++ Main.py
@@ -8,10 +8,13 @@ from Api.Confs.Router import router
 from Api.Exceptions.ExceptionHandler import ExceptionHandler
 
 
+APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT", "development")
+
 app = FastAPI(
   dependencies = [
     Depends(Authorization())
-  ]
+  ],
+  redoc_url = None, docs_url = None if APP_ENVIRONMENT != "development" else "/docs"
 )
 
 app.include_router(router)
@@ -19,7 +22,7 @@ app.add_exception_handler(Exception, ExceptionHandler.throw)
 
 
 if __name__ == "__main__":
-  if (os.getenv("APP_ENVIRONMENT", "development") == "development"):
+  if (APP_ENVIRONMENT == "development"):
     dotenv.load_dotenv(".env")
 
   uvicorn.run("Main:app", host=os.getenv("APP_HOST"), port=int(os.getenv("APP_HOST_PORT")))
